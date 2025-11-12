1956e26e66e54d43ee003ef879796825fb3b4226
Vijay Kumar <wijay.outlook.in>
2025-11-12 07:42:52 +0530

    fix: url updated

--- src/AddBookForm.jsx
+++ src/AddBookForm.jsx
@@ -25,7 +25,7 @@ const AddBookForm = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      const response = await fetch("http://localhost:3000/books", {
+      const response = await fetch("https://book-apis-blond.vercel.app/books", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
